5d78030766a189ba36b1f6a8776452c9805f6f4e
modderme123 <modderme123@gmail.com>
2018-02-24 12:41:44 -0800

    begin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use std::net::{TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::thread;
+use std::collections::HashMap;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+
+enum Action {
+    Add(SocketAddr, TcpStream),
+    Remove(SocketAddr),
+    Broadcast(SocketAddr, String)
+}
+
+struct Server {
+    connections: HashMap<SocketAddr, TcpStream>,
+}
 
 fn handle_client(mut stream: TcpStream) {
     stream.write(b"testing").unwrap();
@@ -16,6 +29,8 @@ fn handle_client(mut stream: TcpStream) {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("try connecting via `telnet localhost 8080`");
+    let (tx, rx): (Sender<Action>, Receiver<Action>) = mpsc::channel();
+
     loop {
         if let Ok((stream, addr)) = listener.accept() {
             thread::spawn(|| {
