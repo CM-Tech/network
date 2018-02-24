478035c5838ddb9414277387fff7fcf124914c96
modderme123 <modderme123@gmail.com>
2018-02-24 12:30:07 -0800

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Write;
+use std::io::{Read, Write};
+use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
     stream.write(b"testing").unwrap();
     stream.flush().unwrap();
+    loop {
+        let mut buf = [0; 4096];
+        if let Ok(n) = stream.read(&mut buf) {
+            stream.write(&buf[0..n]).unwrap();
+        }
+    }
 }
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("try connecting via `telnet localhost 8080`");
-    // accept connections and process them serially
     loop {
         if let Ok((stream, addr)) = listener.accept() {
-            handle_client(stream);
+            thread::spawn(|| {
+                handle_client(stream);
+            });
         }
     }
 }
