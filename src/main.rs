3c6f1ece433a3743befbf49b8485ebae68102f49
modderme123 <modderme123@gmail.com>
2018-02-25 08:22:30 -0800

    messages

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,7 @@ impl Server {
             addr
         );
         println!("{}", msg);
+        self.broadcast(&serde_json::to_string(&Message::Remove(*addr)).unwrap())
     }
 }
 
@@ -71,10 +72,16 @@ fn handle_client(mut stream: TcpStream, addr: SocketAddr, sender: Sender<Action>
     sender.send(Action::Remove(addr)).ok();
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 struct Point {
     x: f64,
     y: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Message {
+    Move(SocketAddr, Point),
+    Remove(SocketAddr),
 }
 
 fn main() {
@@ -136,31 +143,64 @@ fn main() {
         .build()
         .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
     let mut time = 0f32;
-    let mut players = vec![Point { x: 50.0, y: 50.0 }];
+    let local_addr = reader2.local_addr().unwrap();
+    let mut players = HashMap::new();
+    players.insert(local_addr, Point { x: 50.0, y: 50.0 });
+    reader2
+        .write(&serde_json::to_string(&Message::Move(
+            local_addr,
+            players.get(&local_addr).unwrap().clone(),
+        )).unwrap()
+            .as_bytes())
+        .ok();
+    reader2.flush().ok();
     while let Some(e) = window.next() {
         if l {
             if let Ok(message) = rx.try_recv() {
                 match message {
-                    Action::Add(addr, stream) => connections.add_connection(&addr, stream),
+                    Action::Add(addr, mut stream) => {
+                        for (addr, p) in players.iter() {
+                            stream
+                                .write(&serde_json::to_string(&Message::Move(
+                                    *addr,
+                                    players.get(addr).unwrap().clone(),
+                                )).unwrap()
+                                    .as_bytes())
+                                .ok();
+                        }
+                        connections.add_connection(&addr, stream);
+                    }
                     Action::Remove(addr) => connections.remove_connection(&addr),
                     Action::Broadcast(msg) => connections.broadcast(&msg),
                 }
             }
         }
         if let Ok(message) = reader_read.try_recv() {
-            println!("{}", message.len());
-            players[0] = serde_json::from_str(&message).unwrap();
+            match serde_json::from_str(&message).unwrap() {
+                Message::Move(addr, p) => players.insert(addr, p),
+                Message::Remove(addr) => players.remove(&addr),
+            };
         }
-        match e.press_args() {
-            Some(Button::Keyboard(Key::L)) => {
-                players[0].x += 1.0;
-                reader2
-                    .write(&serde_json::to_string(&players[0]).unwrap().as_bytes())
-                    .ok();
-                reader2.flush().ok();
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            if let Some(p) = players.get_mut(&local_addr) {
+                match key {
+                    Key::D => p.x += 1.0,
+                    Key::A => p.x -= 1.0,
+                    Key::S => p.y += 1.0,
+                    Key::W => p.y -= 1.0,
+                    _ => (),
+                }
             }
-            _ => (),
-        };
+            reader2
+                .write(&serde_json::to_string(&Message::Move(
+                    local_addr,
+                    players.get(&local_addr).unwrap().clone(),
+                )).unwrap()
+                    .as_bytes())
+                .ok();
+            reader2.flush().ok();
+        }
+
         window.draw_2d(&e, |c, g| {
             clear(
                 [
@@ -171,7 +211,7 @@ fn main() {
                 ],
                 g,
             );
-            for p in players.iter() {
+            for (_, p) in players.iter() {
                 rectangle(
                     [1.0; 4],
                     [p.x - 25.0, p.y - 25.0, 50.0, 50.0],
